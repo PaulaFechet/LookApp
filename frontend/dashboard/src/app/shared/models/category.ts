d2f77241ee19496fd71846081c8cd5a06ec59626
Paula Fechet <pioana027@gmail.com>
2021-04-28 19:30:18 +0300

    corrected category model

--- frontend/dashboard/src/app/shared/models/category.ts
+++ frontend/dashboard/src/app/shared/models/category.ts
@@ -1,9 +1,12 @@
 import {RecordModel} from './record';
+import { User } from './user';
 
 export class CategoryModel{
   id: number;
-  title: number;
+  title: string;
   description: string;
   type: string;
   records: RecordModel[];
+  creatorId: number;
+  creator?: User;
 }
