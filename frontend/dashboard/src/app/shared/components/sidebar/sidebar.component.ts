57296f198f2ae5e3184824367d0b1c39cce562b9
Paula Fechet <pioana027@gmail.com>
2021-05-02 14:35:19 +0300

    fix sidebar link for categories page

--- frontend/dashboard/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/dashboard/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
