c3b5469411b49ea61982ba6e85745ea7f7df696d
Paula Fechet <pioana027@gmail.com>
2021-05-06 23:15:16 +0300

    update category request

---
+++ backend/LookApp/Dtos/UpdateCategoryRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LookApp.API.Dtos
+{
+    public class UpdateCategoryRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string UnitOfMeasure { get; set; }
+        public double? LowerLimit { get; set; }
+        public double? UpperLimit { get; set; }
+    }
+}
